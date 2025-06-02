2b70327a159100ced63a79e7cf68d4208182ca37
HenryC863 <Gaohongchen80@gmail.com>
2025-06-02 14:26:06 -0700

    Update deploy-to-shinyapps.R

--- app/deploy-to-shinyapps.R
+++ app/deploy-to-shinyapps.R
@@ -1,12 +1,18 @@
+#deploy to shinyapps.io
+#first you will need an account
+
 install.packages("rsconnect")
 library(rsconnect)
 
 rsconnect::setAccountInfo(name='gaohongc',
-                          token='84271148830EF8DC87CFFEE57761EA25',
-                          secret='liPB2B6BEPtDBAOBLShYxffAlGpZgQ/6oDsiSo0l')
+                          token='<hide>',
+                          secret='<SECRET>')
 
 library(rsconnect)
 rsconnect::deployApp(appDir =  "~/Desktop/stat_418/project/shinyapp", 
                      appName = "life_expectancy")
+
+#this is now running at
+#https://nlangholz.shinyapps.io/clusters/
 
 
